6afc64acdd00773f0d705515206ee52842246a79
Lucifer4602 <krishnamahajan4602@gmail.com>
2024-09-03 01:44:34 +0530

    final commit all errors resolved

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors"; // Import cors
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+cloudinary.config({
+  cloud_name: "dwmmgjqpl",
+  api_key: "455592915759865",
+  api_secret: "5AUHhrVs7b8aTECA8RFvsfRk9Ts",
+});
+
+// Middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Configure CORS
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
